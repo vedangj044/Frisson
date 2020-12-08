17c06e5f49223790e9c02289dc1abae292e4113d
Ajay Galagali <gvajay321@gmail.com>
2020-12-08 22:45:38 +0530

    Implemented SplashScreen (#12)
    * Implemented SplashScreen
    
    * Fixed Green border Splash screen icon glitch

--- app/src/main/java/com/vedangj044/frisson/MainActivity.kt
+++ app/src/main/java/com/vedangj044/frisson/MainActivity.kt
@@ -2,10 +2,13 @@ package com.vedangj044.frisson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Frisson)
         setContentView(R.layout.activity_main)
+
     }
 }
