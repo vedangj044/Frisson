dfd819282f59ce668d78d21bf5557df1bc16f98b
spacemonkey1101 <ritambh54@gmail.com>
2020-12-12 15:35:54 +0530

    Added a View Model Class

---
+++ app/src/main/java/com/vedangj044/frisson/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.vedangj044.frisson
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+
+class MainViewModel(private val apiService: APIService) : ViewModel() {
+    val listData = Pager(PagingConfig(pageSize = 6)) {
+        PostDataSource(apiService)
+    }.flow.cachedIn(viewModelScope)
+}
