8620e7f7daf91e46cf32a7a9aad0c300d2de3604
Ritam Bhattacharyya <ritambh54@gmail.com>
2020-12-13 10:44:31 +0530

    View model (#16)
    * Added a View Model Class
    
    * change Gradle version and page size

---
+++ app/src/main/java/com/vedangj044/frisson/MainViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.vedangj044.frisson
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelFactory(private val apiService: APIService) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(apiService) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
