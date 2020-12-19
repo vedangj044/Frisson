43d1212275d4dd09741ed9cb7a5e6ce891968f1b
codeDamon <ash141agarwal@gmail.com>
2020-12-19 23:28:12 +0530

    Removed class(SimplyCount) declaration Now project have a kotlin file named SimplyCount.kt which contains a function getApproximateValue(Long)

--- app/src/main/java/com/vedangj044/frisson/SimplyCount.kt
+++ app/src/main/java/com/vedangj044/frisson/SimplyCount.kt
@@ -1,26 +1,20 @@
 package com.vedangj044.frisson
 
-class SimplyCount {
+public fun getApproximateValue(number: Long): String {
 
-    public fun getApproximateValue(number : Long) : String{
+    var convertedNumber = ""
 
-        var convertedNumber = ""
+    if (number < 1000) {
+        convertedNumber = number.toString() //for number less than one thousand
+    } else if (number < 1000000) {
+        convertedNumber = (number / 1000).toString() + "K" //for number less than a million
+    } else if (number < 1000000000) {
+        convertedNumber = (number / 1000000).toString() + "M" //for number less than a billion
+    } else if (number < 1000000000000) {
+        convertedNumber = (number / 1000000000).toString() + "B" //for number less than a trillion
+    } else
+        convertedNumber =
+            (number / 1000000000000).toString() + "T" //for number equal to or more than a trillion
 
-        if(number < 1000){
-            convertedNumber = number.toString() //for number less than one thousand
-        }
-        else if (number < 1000000){
-            convertedNumber = (number/1000).toString() + "K" //for number less than a million
-        }
-        else if(number < 1000000000){
-            convertedNumber = (number/1000000).toString() + "M" //for number less than a billion
-        }
-        else if(number < 1000000000000){
-            convertedNumber = (number/1000000000).toString() + "B" //for number less than a trillion
-        }
-        else
-            convertedNumber = (number/1000000000000).toString() + "T" //for number equal to or more than a trillion
-
-        return convertedNumber
-    }
+    return convertedNumber
 }
