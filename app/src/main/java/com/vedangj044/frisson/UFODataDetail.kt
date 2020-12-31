8c015105fd458e084df01f2c10af4423b3f1ef45
spacemonkey1101 <ritambh54@gmail.com>
2020-12-31 20:08:07 +0530

    ufo data class

---
+++ app/src/main/java/com/vedangj044/frisson/UFODataDetail.kt
@@ -0,0 +1,50 @@
+package com.vedangj044.frisson
+
+import com.squareup.moshi.Json
+import java.util.*
+
+data class UFODataDetail {
+
+    @Json(name = "city")
+    val city: String,
+
+    @Json(name = "comments")
+    val comments: String,
+
+    @Json(name = "country")
+    val country: String,
+
+    @Json(name = "date added")
+    val date_added: Date,
+
+    @Json(name = "id")
+    val id: Int,
+
+    @Json(name = "image")
+    val image: String,
+
+    @Json(name = "keywords")
+    val keywords: String,
+
+    @Json(name = "lat")
+    val lat: Double,
+
+    @Json(name = "long")
+    val long: Double,
+
+    @Json(name = "seconds")
+    val seconds: Double,
+
+    @Json(name = "shape")
+    val shape: String,
+
+    @Json(name = "sighting_date")
+    val sightingDate: String,
+
+    @Json(name = "state")
+    val state: String,
+
+    @Json(name = "time")
+    val time: String,
+
+}
