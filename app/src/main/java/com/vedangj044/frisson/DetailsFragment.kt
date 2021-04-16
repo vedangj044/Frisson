9383cacabecbef133547b372ff1c562b7529c64a
vedangj044 <vedangj044@gmail.com>
2021-04-16 18:06:40 +0530

    added detail fragment

---
+++ app/src/main/java/com/vedangj044/frisson/DetailsFragment.kt
@@ -0,0 +1,27 @@
+package com.vedangj044.frisson
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.layout_detail_fragment, container, false);
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+
+    }
+
+}
